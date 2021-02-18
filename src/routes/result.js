b6f508008a1448411254667da8950d0dcaae9ea2
한명수 <hmsoo@hanmyeongsuui-MacBookPro.local>
2021-02-18 20:44:45 +0900

    Added Router

---
+++ src/routes/result.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Result extends React.Component {
+
+    render() {
+        return(
+            <span> result</span>
+        );
+    }
+}
+
+export default Result;
